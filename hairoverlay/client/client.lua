24020aac28fca26cef9b0a4ff18645996d076edd
yohananl <dartdartpart@gmail.com>
2024-02-18 22:59:50 -0800

    Fix

--- hairoverlay/client/client.lua
+++ hairoverlay/client/client.lua
@@ -1,4 +1,5 @@
 local QBCore = exports['qb-core']:GetCoreObject()
+local currentHairOverlay = { collection = '', hairoverlay = '' }
 
 RegisterNetEvent('QBCore:Client:OnPlayerLoaded', function()
     TriggerServerEvent('hairoverlay:server:initializeHairOverlay')
@@ -11,17 +12,29 @@ RegisterNetEvent('hairoverlay:client:initializeHairOverlay', function()
         if hairoverlay then
             print(PlayerPedId()) -- The correct player ped id
             AddPedDecorationFromHashes(PlayerPedId(), hairoverlay.collection, hairoverlay.overlay)
+            currentHairOverlay = hairoverlay
         end
     end)
 end)
 
 RegisterCommand('hairoverlay', function(_, args, _)
+    local playerPed = PlayerPedId()
     local index = tonumber(args[1]) or 1
-    local hairoverlay = HairOverlaysMale[index].collection
+    local hairoverlay = HairOverlaysMale[index]
+    local decorations = GetPedDecorations(playerPed)
+
+    ClearPedDecorations(playerPed)
+
+    for _, value in pairs(decorations) do
+        if currentHairOverlay.overlay ~= value[2] then
+            AddPedDecorationFromHashes(playerPed, value[1], value[2])
+        end
+    end
 
     QBCore.Functions.TriggerCallback('hairoverlay:server:cb_setHairOverlay', function(success)
         if success then
-            AddPedDecorationFromHashes(PlayerPedId(), hairoverlay.collection, hairoverlay.overlay)
+            AddPedDecorationFromHashes(playerPed, hairoverlay.collection, hairoverlay.overlay)
+            currentHairOverlay = hairoverlay
             TriggerEvent('QBCore:Notify', "Hair overlay applied", "success")
         end
     end, hairoverlay)
